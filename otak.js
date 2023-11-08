704eac8689454eb0a76029fb31d5c0de3149b6c1
Khofifahifah <khofifahfifah2163@gmail.com>
2023-11-08 15:40:30 +0700

    30 % progress FE

--- otak.js
+++ otak.js
@@ -7,3 +7,32 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('sticky');
     }
 });
+
+// // Tuker Posisi
+// const bandaraAsalSelect = document.getElementById('bandaraAsal');
+// const bandaraTujuanSelect = document.getElementById('bandaraTujuan');
+// const swapButton = document.getElementById('swapButton');
+
+// swapButton.addEventListener('click', function() {
+//   const tempValue = bandaraAsalSelect.value;
+//   bandaraAsalSelect.value = bandaraTujuanSelect.value;
+//   bandaraTujuanSelect.value = tempValue;
+// });
+
+//navbar 
+const navbarItems = document.querySelectorAll('.navbar nav ul li a');
+
+  navbarItems.forEach(item => {
+    item.addEventListener('click', function () {
+      navbarItems.forEach(item => {
+        item.classList.remove('active');
+      });
+
+      item.classList.add('active');
+    });
+  });
+
+
+
+
+
