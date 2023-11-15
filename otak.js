f844406920a6d0e48281d5b0489795eb9c5e419c
Khofifahifah <khofifahfifah2163@gmail.com>
2023-11-15 21:04:32 +0700

    full FE perubahan di navbar & home

--- otak.js
+++ otak.js
@@ -8,29 +8,6 @@ window.addEventListener('scroll', function() {
     }
 });
 
-// // Tuker Posisi
-// const bandaraAsalSelect = document.getElementById('bandaraAsal');
-// const bandaraTujuanSelect = document.getElementById('bandaraTujuan');
-// const swapButton = document.getElementById('swapButton');
-
-// swapButton.addEventListener('click', function() {
-//   const tempValue = bandaraAsalSelect.value;
-//   bandaraAsalSelect.value = bandaraTujuanSelect.value;
-//   bandaraTujuanSelect.value = tempValue;
-// });
-
-//navbar 
-const navbarItems = document.querySelectorAll('.navbar nav ul li a');
-
-  navbarItems.forEach(item => {
-    item.addEventListener('click', function () {
-      navbarItems.forEach(item => {
-        item.classList.remove('active');
-      });
-
-      item.classList.add('active');
-    });
-  });
 
 
 
