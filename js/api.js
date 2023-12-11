48bd1078c28e7522f366fc0954dd44615ef5cec5
AFreborn22 <aafauzan52@gmail.com>
2023-12-11 12:40:44 +0700

    baruuuuu

--- js/api.js
+++ js/api.js
@@ -1,43 +1,46 @@
-document.getElementById('hotelForm').addEventListener('submit', async function (event) {
-  event.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('hotelForm').addEventListener('submit', async function (event) {
+      event.preventDefault();
 
-  const city = document.getElementById('city').value;
+      const city = document.getElementById('city').value;
+      try {
+          const response = await fetch(`http://localhost:3000/hotel?city=${city}`);
+          const hotels = await response.json();
+          const hotelsString = JSON.stringify(hotels);
 
-  try {
-    // Kirim permintaan pencarian ke backend
-    const response = await fetch(`http://localhost:3000/hotel?city=${city}`);
-    const hotels = await response.json();
 
-    // Tampilkan hasil pencarian di dalam elemen dengan id "hotelContainer"
-    const hotelContainer = document.getElementById('hotelContainer');
-    hotelContainer.innerHTML = '';
+          console.log('Response from server:', hotelsString);
 
-    hotels.forEach(hotels => {
-      const hotelBox = document.createElement('div');
-      hotelBox.className = 'hotelbox';
+          const hotelHasil = document.getElementById('hotelHasil');
+          hotelHasil.innerHTML = '';
 
-      hotelBox.innerHTML = `
-        <div class="info-box" id="infohotel">
-          <img src="${hotels.image}" alt="hotel">
-          <div class="info-content">
-              <h2>${hotels.hotel_name}</h2>
-              <p>Rating : ${hotels.rating}</p>
-              <p class="lokasi">${hotels.city}, ${hotels.address}</p>
-              <p>Fasilitas:</p>
-              <div class="fasilitas-container">
-                <div class="fasilitas-item">
-                  <span class="fasilitas-text">${hotels.fasilitas}</span>
-                </div>
-              </div>
-              <p class="harga">Rp.700.000 /malam/kamar</p>
-              <button class="button-booking" onclick="showPopup('Reservasi berhasil dipesan')">Pesan Sekarang</button>
-          </div>
-        </div>
-      `;
+          hotelsString.forEach(hotel => {
+              const hotelBox = document.createElement('div');
+              hotelBox.className = 'hotelbox';
 
-      hotelContainer.appendChild(hotelBox);
-    });
-  } catch (error) {
-    console.error('Error fetching hotels:', error);
-  }
+              hotelBox.innerHTML = `
+                  <div class="info-box" id="infohotel">
+                      <img src="${hotel.image}" alt="hotel">
+                      <div class="info-content">
+                          <h2>${hotel.hotel_name}</h2>
+                          <p>Rating : ${hotel.rating}</p>
+                          <p class="lokasi">${hotel.city}, ${hotel.address}</p>
+                          <p>Fasilitas:</p>
+                          <div class="fasilitas-container">
+                              <div class="fasilitas-item">
+                                  <span class="fasilitas-text">${hotel.fasilitas}</span>
+                              </div>
+                          </div>
+                          <p class="harga">Rp.700.000 /malam/kamar</p>
+                          <button class="button-booking" onclick="showPopup('Reservasi berhasil dipesan')">Pesan Sekarang</button>
+                      </div>
+                  </div>
+              `;
+
+              hotelHasil.appendChild(hotelBox);
+          });
+      } catch (error) {
+          console.error('Error fetching hotels:', error);
+      }
+  });
 });
